3e273c02e77fb9bedca189e7875e2e71dde73aee
Onur Temiz <onurmain@gmail.com>
2020-05-18 13:07:54 +0300

    mend

---
+++ backend/build/precache-manifest.e3ad8cce557b8392992291de9d7eb4f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfe3dd065f944a209bee30fe03ad68e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "210af83692208d211c4f",
+    "url": "/static/css/4.627926fe.chunk.css"
+  },
+  {
+    "revision": "d4964fefff15d73640fa",
+    "url": "/static/js/0.cf771000.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/0.cf771000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5753e8c29441e2c01fcb",
+    "url": "/static/js/3.afd331ad.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/3.afd331ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210af83692208d211c4f",
+    "url": "/static/js/4.dab57d9f.chunk.js"
+  },
+  {
+    "revision": "9243d385dc74daba0ec3",
+    "url": "/static/js/5.6b9e8e67.chunk.js"
+  },
+  {
+    "revision": "f44b835b41bb21a6b2fd",
+    "url": "/static/js/6.647c4798.chunk.js"
+  },
+  {
+    "revision": "2de5639dd31bc5560b1f",
+    "url": "/static/js/main.fe7f0cca.chunk.js"
+  },
+  {
+    "revision": "de57d0a863bb8768ee34",
+    "url": "/static/js/runtime-main.4df336ee.js"
+  }
+]);
